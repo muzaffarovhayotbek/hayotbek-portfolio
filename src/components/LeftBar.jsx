54f24bf17baa6b2604682011ef03d3d4bc74dc5f
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-04-10 14:03:17 +0500

    add

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -3,11 +3,12 @@ import photo from '../assets/photo.jpg';
 import gmail from '../assets/gmail.svg';
 import github from '../assets/gitHub.png';
 import telegram from '../assets/telegram.svg';
+
 function LeftBar() {
   return (
-    <div className="fixed  w-[288px] h-[924px] top-12 gap-4 bg-black   ">
+    <div className="w-[288px] min-h-dvh bg-black">
       <div className="pl-[14px] pt-14">
-        <img src={photo} alt="" width={259} />
+        <img src={photo} alt="profile" width={259} />
         <h2 className="text-[18px] text-[#FFFFFF] pt-4">Hayotbek M.</h2>
         <div className="flex items-center gap-[10px] pl-[14px] mt-[8px] flex-wrap">
           <button className="p-2 bg-[#FFFFFF1A] rounded-[8px]">
@@ -16,22 +17,24 @@ function LeftBar() {
           <button className="p-2 bg-[#FFFFFF1A] rounded-[8px]">Odam</button>
           <button className="p-2 bg-[#FFFFFF1A] rounded-[8px]">Dizayner</button>
         </div>
-        <h2 className="w-full  pt-[14px] border-b-[1px] border-[#FFFFFF40]"></h2>
+        <div className="w-full pt-[14px] border-b border-[#FFFFFF40]"></div>
         <div className="flex flex-col gap-[15px] mt-[15.5px] w-[171px]">
-          <div className="flex gap-[10px] w-[171px]">
+          <div className="flex gap-[10px]">
             <img src={gmail} alt="gmail" width={44} />
             <div className="flex flex-col">
               <h2 className="text-white text-[16px] font-[500] pb-[2px]">
                 E-pochta
               </h2>
-              <p className="text-[#FFFFFFB2] text-[16px]">hmuzfrov@gmail.com</p>
+              <p className="text-[#FFFFFFB2] text-[16px]">
+                hmuzfrov@gmail.com
+              </p>
             </div>
           </div>
 
-          <div className="flex gap-[10px] w-[260px] rounded-[6px] pt-[14px]">
-            <img src={github} alt="gmail" width={44} />
+          <div className="flex gap-[10px] pt-[14px]">
+            <img src={github} alt="github" width={44} />
             <div className="flex flex-col">
-              <h2 className="text-white text-[16px] font-[500] pb-[2px]]">
+              <h2 className="text-white text-[16px] font-[500] pb-[2px]">
                 Github
               </h2>
               <p className="text-[#FFFFFFB2] text-[14px] font-[400]">
@@ -40,10 +43,10 @@ function LeftBar() {
             </div>
           </div>
 
-          <div className="flex gap-[10px] w-[260px] rounded-[6px] pt-[14px]">
-            <img src={telegram} alt="gmail" width={44} />
+          <div className="flex gap-[10px] pt-[14px]">
+            <img src={telegram} alt="telegram" width={44} />
             <div className="flex flex-col">
-              <h2 className="text-white text-[16px] font-[500] pb-[2px]]">
+              <h2 className="text-white text-[16px] font-[500] pb-[2px]">
                 Telegram
               </h2>
               <p className="text-[#FFFFFFB2] text-[14px] font-[400]">
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -6,51 +6,56 @@ import LeftBar from '../components/LeftBar';
 
 function MainLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-dvh flex-row">
+      {/* Left sidebar */}
       <LeftBar />
-      <header className="pt-[8px] pb-[8px] bg-[#1B1B1B] pl-[14px] pr-[14px]">
-        <div className="container mx-auto flex items-center justify-between">
-          <div className="flex items-center gap-[14px]">
-            <img src={left} alt="" className="w-[32px] p-[2px] gap-[10px]" />
-            <Link
-              to="/"
-              className="text-[22px] text-[#FFFFFF] leading-[28px] text-center"
-            >
-              HAYOTBEK.{''}
-              <span className="text-[22px] text-[#39965F] text-center leading-[28px]">
-                UZ
-              </span>
-            </Link>
-          </div>
-          <div className="flex items-center gap-[14px]">
-            <ul className="flex items-center gap-[24px] text-[16px] text-white leading-[100%] font-[400]">
-              <NavLink to="/">
-                <li>Bosh sahifa</li>
-              </NavLink>
-              <NavLink to="/about">
-                <li>Haqida</li>
-              </NavLink>
-              <NavLink to="/projects">
-                <li>Loyihalar</li>
-              </NavLink>
-              <NavLink to="/contact">
-                <li>Bog'lanish</li>
-              </NavLink>
-            </ul>
-            <div className="header-git">
-              <a href="https://github.com/muzaffarovhayotbek">
-                <img src={github} alt="github" width={32} />
-              </a>
+
+      {/* Main content */}
+      <div className="flex flex-col flex-grow">
+        <header className="pt-[8px] pb-[8px] bg-[#1B1B1B] pl-[14px] pr-[14px]">
+          <div className="container mx-auto flex items-center justify-between">
+            <div className="flex items-center gap-[14px]">
+              <img src={left} alt="" className="w-[32px] p-[2px] gap-[10px]" />
+              <Link
+                to="/"
+                className="text-[22px] text-[#FFFFFF] leading-[28px] text-center"
+              >
+                HAYOTBEK.
+                <span className="text-[22px] text-[#39965F] leading-[28px]">
+                  UZ
+                </span>
+              </Link>
+            </div>
+            <div className="flex items-center gap-[14px]">
+              <ul className="flex items-center gap-[24px] text-[16px] text-white leading-[100%] font-[400]">
+                <NavLink to="/">
+                  <li>Bosh sahifa</li>
+                </NavLink>
+                <NavLink to="/about">
+                  <li>Haqida</li>
+                </NavLink>
+                <NavLink to="/projects">
+                  <li>Loyihalar</li>
+                </NavLink>
+                <NavLink to="/contact">
+                  <li>Bog'lanish</li>
+                </NavLink>
+              </ul>
+              <div className="header-git">
+                <a href="https://github.com/muzaffarovhayotbek">
+                  <img src={github} alt="github" width={32} />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </header>
+        </header>
 
-      <main className="flex-grow">{children}</main>
+        <main className="flex-grow">{children}</main>
 
-       <footer className="bg-[#1B1B1B] text-white text-center py-2">
-        <h2>HAYOTBEK.UZ</h2>
-      </footer>
+        <footer className="bg-[#1B1B1B] text-white text-center py-2">
+          <h2>HAYOTBEK.UZ</h2>
+        </footer>
+      </div>
     </div>
   );
 }
